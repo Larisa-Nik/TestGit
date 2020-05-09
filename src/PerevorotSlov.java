d68664b4681dadd10dc9109485e83f919359ca41
Larisa-Nik <larisani12@gmail.com>
2020-05-09 15:38:24 +0300

    PerevorotSlov

---
+++ src/PerevorotSlov.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; // импорт сканера
+
+public class PerevorotSlov {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, i, b;
+        String str = scan.nextLine();
+        a = str.length();
+        b = a;
+        
+        for (i = 0; i < a; i++) {
+            b = b - 1;
+            char c = str.charAt(b);
+            System.out.print(c);
+
+
+        }
+    }
+
+}

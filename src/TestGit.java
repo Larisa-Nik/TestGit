891b3ee04bde1fc7f84346f5da4e97a3dc4d2fe8
Larisa-Nik <larisani12@gmail.com>
2020-05-09 11:51:20 +0300

    change!!!

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,18 +1,19 @@
 
   import java.util.Scanner; // импорт сканера
   public class TestGit {
-        public static void main(String[] args) {
+      public static void main(String[] args) {
 
-            Scanner scan = new Scanner(System.in);
+          Scanner scan = new Scanner(System.in);
 
-            String name1 = scan.nextLine();
-            String name2 = scan.nextLine();
+          String name1 = scan.nextLine();
+          String name2 = scan.nextLine();
 
-            if (name1.equals(name2)) System.out.println("Имена идентичны.");  //name1.equals(name2) - сравнение слов посимвольно
-            else if (name1.length()==name2.length()) System.out.println("Длины имен равны.");  //name1.length() - возвращает количество символов
-            else if (name1.equals(name2)==false&&name1.length()!=name2.length()) System.out.print("");
+          if (name1.equals(name2))
+              System.out.println("Имена идентичны.");  //name1.equals(name2) - сравнение слов посимвольно
+          else if (name1.length() == name2.length())
+              System.out.println("Длины имен равны.");  //name1.length() - возвращает количество символов
+          else if (name1.equals(name2) == false && name1.length() != name2.length()) System.out.print("");
 
-        }
-
-
-    }
+      }
+  }
+// for new sohranenie

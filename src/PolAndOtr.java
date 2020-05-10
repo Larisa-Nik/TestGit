d1804fc9852f244d51ea6eef202eaaba60dc0d4f
Larisa-Nik <larisani12@gmail.com>
2020-05-10 11:12:41 +0300

    PolojitelnoeChislo

---
+++ src/PolAndOtr.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PolAndOtr {
+    //ввести с клавы три целых числа. Вывести на экран количество положительных чисел среди этих трех b и количество отрицательных.
+//если положительных нету, программа должна вывести "0". Причем "0" не есть положительным ни отрицательным-- нейтральное
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int a, i, kol = 0, kol1=0;
+
+            for (i = 0; i < 3; i++) {
+                a = scan.nextInt();    // ввод числа с клавиатуры от 1 до 999
+                if (a > 0) kol = kol + 1;
+                else  if (a<0) kol1=kol1+1;
+            }
+
+            System.out.println( "количество положительных чисел: " +kol);
+                System.out.print( "количество отрицательных чисел: " +kol1);
+
+        }
+    }
+
+

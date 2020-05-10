13b7d49395df74f88f5e19aa39638212d3057703
Larisa-Nik <larisani12@gmail.com>
2020-05-10 18:29:11 +0300

    TablUmnoj

---
+++ src/TablUmnoj.java
@@ -0,0 +1,11 @@
+public class TablUmnoj {
+    public static void main(String[] args) {
+        int a = 1,b=1,c;
+        while (b <= 10){
+        while (a <= 10) {c=a*b;
+            System.out.print(c+" ");
+            a++; } System.out.println(); b++; a=1;
+    }
+}
+}
+

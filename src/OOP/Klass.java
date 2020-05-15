0d5e14daa96737cab44b0a73629d765b5ffb9120
Larisa-Nik <larisani12@gmail.com>
2020-05-15 21:22:11 +0300

    Summirovanie

--- src/OOP/Klass.java
+++ src/OOP/Klass.java
@@ -1,4 +1,24 @@
 package OOP;
 
+import java.util.Scanner;
+
 public class Klass {
-}
+
+/*
+Суммирование
+*/
+        public static void main(String[] args) throws Exception {
+            Scanner scan = new Scanner(System.in);
+            int data = 0;
+            while (true) {
+                int d = scan.nextInt();
+                if (d != -1) data = data + d;
+               else {data = data + d; break;}
+            }
+
+            System.out.println(data);
+        }
+
+    }
+
+

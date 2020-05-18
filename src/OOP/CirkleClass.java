49bd263c0b94c96603a31c57e7b3191bbd94e23e
Larisa-Nik <larisani12@gmail.com>
2020-05-18 20:55:08 +0300

    CircleClass

---
+++ src/OOP/CirkleClass.java
@@ -0,0 +1,40 @@
+package OOP;
+
+public class CirkleClass {
+    public static void main(String[] args) {
+    }
+
+    public class  Cirkle {
+        int centerX;
+        int centerY;
+        int radius;
+        int color;
+        int width;
+
+       public Cirkle(int centerX1, int centerY1, int radius1 ){
+        centerX=centerX1;
+        centerY=centerY1;
+        radius = radius1;
+       }
+        public Cirkle(int centerX1, int centerY1, int radius1, int width1 ){
+            centerX=centerX1;
+            centerY=centerY1;
+            radius = radius1;
+            width=width1;
+        }
+        public Cirkle(int centerX1, int centerY1, int radius1, int width1, int color1 ){
+            centerX=centerX1;
+            centerY=centerY1;
+            radius = radius1;
+            width=width1;
+            color=color1;
+        }
+
+    }
+
+
+
+
+
+
+}

535babaa43fe1756b06c40c039dcda1ae955481f
Larisa-Nik <larisani12@gmail.com>
2020-05-20 22:57:54 +0300

    RectangleClass

---
+++ src/OOP/RectangleClass.java
@@ -0,0 +1,45 @@
+package OOP;
+
+public class RectangleClass {
+    public static void main(String[] args) {
+    }
+
+    class Rectangle{
+        int top;
+        int left;
+        int width;
+        int height;
+
+        public Rectangle(int topR,int leftR, int widthR,int heightR){
+            top=topR;
+            left=leftR;
+            width=widthR;
+            height=heightR; }
+
+        public Rectangle(int topR,int leftR, int widthR){
+            top=topR;
+            left=leftR;
+            width=widthR;
+             }
+
+        public Rectangle(int topR,int leftR){
+            top=topR;
+            left=leftR;
+             }
+        public Rectangle(int topR){
+            top=topR;
+            }
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+

1da46ea62b322a8b07d453b521683b5a9c0b4afe
Larisa-Nik <larisani12@gmail.com>
2020-05-20 22:59:35 +0300

    KonstuktorKotov

--- src/OOP/KonstuktorKotov.java
+++ src/OOP/KonstuktorKotov.java
@@ -1,15 +1,14 @@
 package OOP;
-
-public class KonstuktorKotov {
+ class KonstuktorKotov {
     public static void main(String[] args) {
-Cats cats=new Cats();
-        Cats cats0=new Cats("Vaska", 3,4);
-        Cats cats1=new Cats("Muska",3);
-        Cats cats2=new Cats("Tom");
-        Cats cats3=new Cats(3,"black","Odessa");
+
+        Cats cats0 = new Cats("Vaska", 3, 4);
+        Cats cats1 = new Cats("Muska", 3);
+        Cats cats2 = new Cats("Tom");
+        Cats cats3 = new Cats(3, "black", "Odessa");
     }
 
-   public static class Cats{
+    public static class Cats {
         String name;
         int age;
         int weight;
@@ -17,34 +16,35 @@ Cats cats=new Cats();
         String color;
 
 
-        public Cats(String nameCat){
-            name=nameCat;
-            age=3;
-            weight=4;
-            color="black";}
+        public Cats(String nameCat) {
+            name = nameCat;
+            age = 3;
+            weight = 4;
+            color = "black";
+        }
 
-            public Cats(String nameCat,int ageCat, int weightCat){
-            name=nameCat;
-            age=ageCat;
-            weight=weightCat;
-            color="black";}
+        public Cats(String nameCat, int ageCat, int weightCat) {
+            name = nameCat;
+            age = ageCat;
+            weight = weightCat;
+            color = "black";
+        }
 
-       public Cats(String nameCat,int ageCat){
-           name=nameCat;
-           age=ageCat;
-           weight=4;
-           color="black";}
+        public Cats(String nameCat, int ageCat) {
+            name = nameCat;
+            age = ageCat;
+            weight = 4;
+            color = "black";
+        }
 
-       public Cats(int weightCat,String colorCat, String addressCat){
-           address=addressCat;
-           age=3;
-           weight=weightCat;
-           color=colorCat;}
+        public Cats(int weightCat, String colorCat, String addressCat) {
+            address = addressCat;
+            age = 3;
+            weight = weightCat;
+            color = colorCat;
+        }
 
-       public Cats() {
-
-       }
-   }
-   }
+    }
+}
 
 

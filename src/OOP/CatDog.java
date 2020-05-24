c0e0fc29b764b3a28dff38fc1cc471163ebfc004
Larisa-Nik <larisani12@gmail.com>
2020-05-24 11:30:46 +0300

    CatDog

---
+++ src/OOP/CatDog.java
@@ -0,0 +1,33 @@
+package OOP;
+
+public class CatDog {
+    public static void main(String[] args) {
+        Duck duck1=new Duck();
+        Duck duck2=new Duck();
+        System.out.println(duck1);
+        System.out.println(duck2);
+        Cat cat1=new Cat();
+        Cat cat2=new Cat();
+        System.out.println(cat1);
+        System.out.println(cat2);
+        Dog dog1=new Dog();
+        Dog dog2=new Dog();
+        System.out.println(dog1);
+        System.out.println(dog2);
+    }
+    public static class Cat{
+        public  String toString(){
+            return "Cat";
+    }}
+    public static class Dog{
+        public  String toString(){
+            return "Dog";
+    }}
+    public static class Duck{
+public  String toString(){
+    return "Duck";
+}}
+    }
+
+
+

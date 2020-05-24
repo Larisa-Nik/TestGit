c67b10d93a23fbc33bb6b79f774bc91a6532420a
Larisa-Nik <larisani12@gmail.com>
2020-05-24 23:47:52 +0300

    KamenChik

--- src/OOP/KamenChik.java
+++ src/OOP/KamenChik.java
@@ -3,30 +3,27 @@ package OOP;
 public class KamenChik {
 
     public static void main(String[] args) {
-        Mens mens=new Mens(50);
-        Kamen kamen =new Kamen(34);
-        boolean a= mens.SmogPodnKamen(kamen);
+        Mens mens = new Mens(50);
+        Kamen kamen = new Kamen(34);
+        boolean a = mens.SmogPodnKamen(kamen);
         System.out.println(a);
     }
-    public static class Mens {
+}
+     class Mens {
         int gruzPodn;
 
         public Mens(int gruzPodnMens) {
             gruzPodn = gruzPodnMens;
         }
-
         public boolean SmogPodnKamen(Kamen kamen) {
             if (gruzPodn > kamen.wes  ) return true;
             return false;
         }
     }
-
-    public   static class   Kamen {
+     class   Kamen {
         int wes;
-
         public Kamen(int wesMens) {
             wes = wesMens;
         }
     }
 
-}

5ed14cfb606994d695c385615b85f9ce0587768e
Larisa-Nik <larisani12@gmail.com>
2020-05-24 11:54:44 +0300

    GetAndSet1

--- src/OOP/GetAndSet1.java
+++ src/OOP/GetAndSet1.java
@@ -5,29 +5,35 @@ public class GetAndSet1 {
         Person1 person = new Person1();
 
 
-}
-  public static class  Person1{
+    }
+
+    public static class Person1 {
         private String name;
         private int age;
         private char sex;
 
-        public  void setName(String userName){
-        name=userName;
+        public void setName(String userName) {
+            name = userName;
         }
-        public String getName(){
+
+        public String getName() {
             return name;
         }
-        public void setAge(int userAge){
-            age=userAge;
+
+        public void setAge(int userAge) {
+            age = userAge;
         }
-        public int getAge(){
+
+        public int getAge() {
             return age;
         }
-        public void setSex(char userSex){
-            sex=userSex;
+
+        public void setSex(char userSex) {
+            sex = userSex;
         }
-        public char getSex(){
+
+        public char getSex() {
             return sex;
         }
     }
-    }
+}
